ddcfe0bbfcbb20627973ccf947b57386ea537fed
Jhon Jairo Urueta Alvarez <jhon.urueta@misena.edu.co>
2023-07-28 11:52:44 -0500

    Update Tema1.vue

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -8,10 +8,8 @@
       h1 Introducción
     .row.align-items-center
       .col-lg-5.mb-4.mb-lg-0(data-aos="fade-right")
-        figure.mb-3
+        figure
           img(src='@/assets/curso/tema1/img1.png' style="width: 470px" alt='').m-auto
-        a.ms-3(href="https://www.freepik.es/foto-gratis/mujeres-caminan-bebiendo-agua-dulce-bosque_3709524.htm#query=oferta%20h%C3%ADdrica%20en%20colombia&position=2&from_view=search&track=robertav1_2_sidre" target="_blank")
-          span #[b.txt-c1.bg-c13.px-3.text-white Fuente]
       .col-lg-7(data-aos="fade-left")
         p El día mundial del agua se celebra el 22 de marzo, como una oportunidad para reflexionar acerca de la importancia de este líquido vital. En Colombia, existe una gran oferta hídrica derivada de nuestros ecosistemas de páramo que alimentan en todo el territorio nacional a los ríos, humedales, lagos y lagunas.  El suministro de agua potable a una población determinada en un territorio permite el desarrollo de actividades domésticas, industriales, agropecuarias, comerciales y de servicios. El acceso a este preciado líquido en las condiciones adecuadas garantiza también la prevención de múltiples enfermedades relacionadas con la deficiencia en los sistemas de agua potable y saneamiento básico en algunas regiones en Colombia y el mundo.
         .cajon.color-primario.p-4
